eb389fd9d4eee376be159415c938155cb9b6f602
H82M19\Student <Student>
2025-08-23 14:19:13 +0700

    Cau 1

---
+++ 22731841_NguyenVanKhaiTien_Tuan1/src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function hello(name) {
+    return `Hello, ${name}!`;
+}
+console.log(hello("200Labaa"));
+//# sourceMappingURL=index.js.map
---
+++ 22731841_NguyenVanKhaiTien_Tuan1/src/index.ts
@@ -0,0 +1,16 @@
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  displayInfo(): void {
+    console.log(`Name: ${this.name}, Age: ${this.age}`);
+  }
+}
+
+const p1 = new Person("Nguyen Van Khai Tien", 21);
+p1.displayInfo()
