302a1aac8825b0597d453c71195bd04d8178ce28
H82M19\Student <Student>
2025-08-23 14:26:10 +0700

    Cau 2

--- 22731841_NguyenVanKhaiTien_Tuan1/src/index.ts
+++ 22731841_NguyenVanKhaiTien_Tuan1/src/index.ts
@@ -14,3 +14,18 @@ class Person {
 
 const p1 = new Person("Nguyen Van Khai Tien", 21);
 p1.displayInfo()
+
+class Student extends Person{
+  grade: string;
+
+  constructor(name: string, age: number, grade: string) {
+    super(name, age);
+    this.grade = grade;
+  }
+  displayAllInfo(): void {
+    console.log(`Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`);
+  }
+}
+
+const s2 = new Student("Nguyen Van A", 20, "A");
+s2.displayAllInfo();
