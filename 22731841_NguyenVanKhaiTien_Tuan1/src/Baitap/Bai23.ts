c15e32de220b96a7fd090fb4354ba788e4e21ec4
KhaiTien <khaitien600@gmail.com>
2025-08-26 11:45:47 +0700

    Cau 23

---
+++ 22731841_NguyenVanKhaiTien_Tuan1/src/Baitap/Bai23.ts
@@ -0,0 +1,19 @@
+interface Payment{
+    pay(amount: number): void;
+}
+
+class CashPayment implements Payment{
+    pay(amount: number): void {
+        console.log(`Paid $${amount} in cash.`);
+    }
+}
+class CardPayment implements Payment {
+  pay(amount: number): void {
+    console.log(`Paid $${amount} in card.`);
+  }
+}
+const cash: Payment = new CashPayment();
+cash.pay(100);  
+
+const card: Payment = new CardPayment();
+card.pay(250); 
