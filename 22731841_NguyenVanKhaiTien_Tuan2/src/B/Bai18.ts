ad7175fe267f018e98190f79a5cb565161c331fc
KhaiTien <khaitien600@gmail.com>
2025-09-04 11:28:49 +0700

    Bai 19

--- 22731841_NguyenVanKhaiTien_Tuan2/src/B/Bai18.ts
+++ 22731841_NguyenVanKhaiTien_Tuan2/src/B/Bai18.ts
@@ -1,5 +1,5 @@
 
-async function fetchUser(id: number): Promise<{ id: number; name: string }> {
+export async function fetchUser(id: number): Promise<{ id: number; name: string }> {
   await new Promise((resolve) => setTimeout(resolve, 1000));
   return {
     id,
---
+++ 22731841_NguyenVanKhaiTien_Tuan2/src/B/Bai19.ts
@@ -0,0 +1,19 @@
+async function fetchUser(id: number): Promise<{ id: number; name: string }> {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return { id, name: `User_${id}` };
+}
+
+async function fetchUsersSequential(ids: number[]) {
+  const results = [];
+  for (const id of ids) {
+    const user = await fetchUser(id);
+    results.push(user);
+  }
+  return results;
+}
+
+async function run() {
+  console.log(await fetchUsersSequential([1, 2, 3]));
+}
+
+run();
